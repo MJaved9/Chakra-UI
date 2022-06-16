527e8bcff3acbd3428a31c928af5c5cd1346638f
MJaved9 <101566706+MJaved9@users.noreply.github.com>
2022-06-17 01:18:05 +0530

    Update App.js

--- Chakra-UI/src/App.js
+++ Chakra-UI/src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Card from './components/Card';
+import Form from './components/Form';
+import SimpleSidebar from './components/Slider';
+
 
 function App() {
   return (
-    
+   <div>
     <Navbar/>
-     
+    < SimpleSidebar></SimpleSidebar>
+    <div className='flex-card'>
+    <Card></Card>
+    <Form></Form>
+    </div>
+     </div>
   );
 }
 
